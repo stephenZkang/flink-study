0238e4fe1e31835b27c901685c2298ef89c0975d
qiaok <qiaok@superred.com.cn>
2023-10-25 17:39:41 +0800

    feat(增加增量聚合): 增加增量聚合
    增加增量聚合

--- src/main/java/com/superred/flink/demo/Main.java
+++ src/main/java/com/superred/flink/demo/Main.java
@@ -32,6 +32,7 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
 import org.apache.flink.streaming.api.functions.co.CoMapFunction;
 import org.apache.flink.streaming.api.functions.co.ProcessJoinFunction;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
 import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.watermark.Watermark;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
@@ -66,7 +67,61 @@ public class Main  {
 //        streamingUnion();
 //        streamingBroadcast();
 //        streamingWindowWatermark();
-        streamingWindowWatermark2();
+//        streamingWindowWatermark2();
+        socketDemoFullCount(args);
+    }
+
+
+    /**
+     * 增量聚合
+     *
+     * @throws Exception
+     */
+    public static void socketDemoFullCount(String[] args) throws Exception {
+        //获取需要的端口号
+        int port;
+        try {
+            ParameterTool parameterTool = ParameterTool.fromArgs(args);
+            port = parameterTool.getInt("port");
+        }catch (Exception e){
+            System.err.println("No port set. use default port 9000--java");
+            port = 9000;
+        }
+
+        //获取flink的运行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        String hostname = "10.66.77.35";
+        String delimiter = "\n";
+        //连接socket获取输入的数据
+        DataStreamSource<String> text = env.socketTextStream(hostname, port, delimiter);
+
+        DataStream<Tuple2<Integer,Integer>> intData = text.map(new MapFunction<String, Tuple2<Integer,Integer>>() {
+            @Override
+            public Tuple2<Integer,Integer> map(String value) throws Exception {
+                return new Tuple2<>(1,Integer.parseInt(value));
+            }
+        });
+
+        intData.keyBy(0)
+                .timeWindow(Time.seconds(5))
+                .process(new ProcessWindowFunction<Tuple2<Integer,Integer>, String, Tuple, TimeWindow>() {
+                    @Override
+                    public void process(Tuple key, Context context, Iterable<Tuple2<Integer, Integer>> elements, Collector<String> out)
+                            throws Exception {
+                        System.out.println("执行process。。。");
+                        long count = 0;
+                        for(Tuple2<Integer,Integer> element: elements){
+                            count++;
+                        }
+                        out.collect("window:"+context.window()+",count:"+count);
+                    }
+                }).print();
+
+
+        //这一行代码一定要实现，否则程序不执行
+        env.execute("Socket window count");
+
     }
 
     /**

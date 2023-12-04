e8264008ed70ea0dffa746923e367611ba84fb3a
qiaok <qiaok@superred.com.cn>
2023-12-04 15:07:09 +0800

    feat(增加增量聚合): 增加增量聚合
    增加增量聚合

--- src/main/java/com/superred/flink/demo/Main.java
+++ src/main/java/com/superred/flink/demo/Main.java
@@ -15,6 +15,7 @@ import com.superred.flink.demo.utils.KafkaTopic;
 import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.JoinFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
@@ -23,11 +24,13 @@ import org.apache.flink.api.java.tuple.Tuple1;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple4;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.ConnectedStreams;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
 import org.apache.flink.streaming.api.functions.co.CoMapFunction;
@@ -68,9 +71,154 @@ public class Main  {
 //        streamingBroadcast();
 //        streamingWindowWatermark();
 //        streamingWindowWatermark2();
-        socketDemoFullCount(args);
+//        socketDemoFullCount(args);
+        socketWindowWordCountJavaCheckPoint(args);
     }
 
+    /**
+     * 增量聚合
+     *
+     * @throws Exception
+     */
+    public static void socketWindowWordCountJavaCheckPoint(String[] args) throws Exception {
+        //获取需要的端口号
+        int port;
+        try {
+            ParameterTool parameterTool = ParameterTool.fromArgs(args);
+            port = parameterTool.getInt("port");
+        }catch (Exception e){
+            System.err.println("No port set. use default port 9000--java");
+            port = 9000;
+        }
+
+        //获取flink的运行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // 每隔1000 ms进行启动一个检查点【设置checkpoint的周期】
+        env.enableCheckpointing(1000);
+        // 高级选项：
+        // 设置模式为exactly-once （这是默认值）
+        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+        // 确保检查点之间有至少500 ms的间隔【checkpoint最小间隔】
+        env.getCheckpointConfig().setMinPauseBetweenCheckpoints(500);
+        // 检查点必须在一分钟内完成，或者被丢弃【checkpoint的超时时间】
+        env.getCheckpointConfig().setCheckpointTimeout(60000);
+        // 同一时间只允许进行一个检查点
+        env.getCheckpointConfig().setMaxConcurrentCheckpoints(1);
+        // 表示一旦Flink处理程序被cancel后，会保留Checkpoint数据，以便根据实际需要恢复到指定的Checkpoint【详细解释见备注】
+        //ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION:表示一旦Flink处理程序被cancel后，会保留Checkpoint数据，以便根据实际需要恢复到指定的Checkpoint
+        //ExternalizedCheckpointCleanup.DELETE_ON_CANCELLATION: 表示一旦Flink处理程序被cancel后，会删除Checkpoint数据，只有job执行失败的时候才会保存checkpoint
+        env.getCheckpointConfig().enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+
+
+        //设置statebackend
+
+        //env.setStateBackend(new MemoryStateBackend());
+        //env.setStateBackend(new FsStateBackend("hdfs://hadoop100:9000/flink/checkpoints"));
+        //env.setStateBackend(new RocksDBStateBackend("hdfs://hadoop100:9000/flink/checkpoints",true));
+
+        String hostname = "10.66.77.35";
+        String delimiter = "\n";
+        //连接socket获取输入的数据
+        DataStreamSource<String> text = env.socketTextStream(hostname, port, delimiter);
+
+        // a a c
+
+        // a 1
+        // a 1
+        // c 1
+        DataStream<WordWithCount> windowCounts = text.flatMap(new FlatMapFunction<String, WordWithCount>() {
+                    public void flatMap(String value, Collector<WordWithCount> out) throws Exception {
+                        String[] splits = value.split("\\s");
+                        for (String word : splits) {
+                            out.collect(new WordWithCount(word, 1L));
+                        }
+                    }
+                }).keyBy("word")
+                .timeWindow(Time.seconds(2), Time.seconds(1))//指定时间窗口大小为2秒，指定时间间隔为1秒
+                .sum("count");//在这里使用sum或者reduce都可以
+                /*.reduce(new ReduceFunction<WordWithCount>() {
+                                    public WordWithCount reduce(WordWithCount a, WordWithCount b) throws Exception {
+
+                                        return new WordWithCount(a.word,a.count+b.count);
+                                    }
+                                })*/
+        //把数据打印到控制台并且设置并行度
+        windowCounts.print().setParallelism(1);
+
+        //这一行代码一定要实现，否则程序不执行
+        env.execute("Socket window count");
+
+    }
+
+    /**
+     * 增量聚合
+     *
+     * @throws Exception
+     */
+    public static void socketWindowWordCountJava(String[] args) throws Exception {
+        //获取需要的端口号
+        int port;
+        try {
+            ParameterTool parameterTool = ParameterTool.fromArgs(args);
+            port = parameterTool.getInt("port");
+        }catch (Exception e){
+            System.err.println("No port set. use default port 9000--java");
+            port = 9000;
+        }
+
+        //获取flink的运行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        String hostname = "10.66.77.35";
+        String delimiter = "\n";
+        //连接socket获取输入的数据
+        DataStreamSource<String> text = env.socketTextStream(hostname, port, delimiter);
+
+        // a a c
+        // a 1
+        // a 1
+        // c 1
+        DataStream<WordWithCount> windowCounts = text.flatMap(new FlatMapFunction<String, WordWithCount>() {
+                    public void flatMap(String value, Collector<WordWithCount> out) throws Exception {
+                        String[] splits = value.split("\\s");
+                        for (String word : splits) {
+                            out.collect(new WordWithCount(word, 1L));
+                        }
+                    }
+                }).keyBy("word")
+                .timeWindow(Time.seconds(2), Time.seconds(1))//指定时间窗口大小为2秒，指定时间间隔为1秒
+                .sum("count");//在这里使用sum或者reduce都可以
+                /*.reduce(new ReduceFunction<WordWithCount>() {
+                                    public WordWithCount reduce(WordWithCount a, WordWithCount b) throws Exception {
+
+                                        return new WordWithCount(a.word,a.count+b.count);
+                                    }
+                                })*/
+        //把数据打印到控制台并且设置并行度
+        windowCounts.print().setParallelism(1);
+
+        //这一行代码一定要实现，否则程序不执行
+        env.execute("Socket window count");
+
+    }
+
+    public static class WordWithCount{
+        public String word;
+        public long count;
+        public  WordWithCount(){}
+        public WordWithCount(String word,long count){
+            this.word = word;
+            this.count = count;
+        }
+        @Override
+        public String toString() {
+            return "WordWithCount{" +
+                    "word='" + word + '\'' +
+                    ", count=" + count +
+                    '}';
+        }
+    }
 
     /**
      * 增量聚合
